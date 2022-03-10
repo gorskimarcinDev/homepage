d7d1e9db0bc3aaa7c8586e3d09edf6fec89c7b10
Marcin Górski <gorskimarcin.dev@gmail.com>
2022-03-10 14:59:15 +0100

    BEM, navigation changes

--- main.js
+++ main.js
@@ -1,1 +1,5 @@
 console.log('Cześć, jestem Marcin. Miło Cię widzieć :)')
+
+const emptyParagraph = document.querySelector('.article__description--js');
+
+emptyParagraph.innerHTML = `Ten tekst jest wstrzykiwany przez JS :)`;
